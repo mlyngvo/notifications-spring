20a7a6a4d0b4395b31828b0d64ab8e0d5c7f4fba
mlyngvo <contact@minhlynguyenvo.com>
2026-02-25 13:26:13 +0100

    chore: jitpack setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,31 +4,16 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 plugins {
     id("org.springframework.boot") version "3.2.3"
     id("io.spring.dependency-management") version "1.1.4"
-    id("maven-publish")
+
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.spring") version "1.9.22"
-}
 
-repositories {
-    mavenCentral()
+    `maven-publish`
 }
 
 publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/mlyngvo/notifications-spring")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
     publications {
         create<MavenPublication>("maven") {
-            groupId = "com.mlyngvo"
-            artifactId = "notifications-spring"
-
             from(components["java"])
         }
     }
@@ -36,12 +21,17 @@ publishing {
 
 group = "com.mlyngvo"
 
+repositories {
+    mavenCentral()
+}
+
 tasks.getByName<BootJar>("bootJar") {
     enabled = false
 }
 
 tasks.getByName<Jar>("jar") {
     enabled = true
+    archiveClassifier.set("")
 }
 
 tasks.withType<KotlinCompile> {
